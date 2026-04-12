7f83f0861017c5e53c33ed998192c79755454326
Decayy25 <mochrizqihermawan615@gmail.com>
2026-04-12 11:07:00 +0700

    deployment

--- server/config/db.js
+++ server/config/db.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 
-const uri = process.env.MONGO_URI || "mongodb://localhost:27017/";
+const uri = process.env.MONGO_URI;
 
 const client = new MongoClient(uri, {
   serverSelectionTimeoutMS: 10000,

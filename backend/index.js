b05800116f5bd2e747fe474b64b5bcf47a5a2ca8
Decayy25 <mochrizqihermawan615@gmail.com>
2026-06-01 20:10:10 +0700

    fix: typo

--- backend/index.js
+++ backend/index.js
@@ -7,64 +7,68 @@ import { addProduct, getProduct, putProduct, delProduct } from "./controller/pro
 
 
 const app = new Elysia()
-    .use(cors({
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true,
+  .use(
+    cors({
+      origin: "https://e-commerce-central-gpu.vercel.app",
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
     }),
-    )
+  )
 
-    .get("/", () => ({
-        status: "Online",
-        message: "Elysia Backend is running perfectly!",
-        database: MONGO_URI ? "Connected (Env OK)" : "Missing Env",
-    }))
+  .get("/", () => ({
+    status: "Online",
+    message: "Elysia Backend is running perfectly!",
+    database: MONGO_URI ? "Connected (Env OK)" : "Missing Env",
+  }))
 
-    .group("/api", (app) =>
+  .group("/api", (app) =>
+    app
+      .group("/auth", (app) =>
         app
-        .group("/auth", (app) =>
-            app
-            .post("/login", async ({ body }) => await login(body))
-            .post("/register", async ({ body }) => await register(body))
-            .get("/verify-email", async ({ query }) => await verifyEmail(query.token))
-            .get("/accounts", async () => await getAccounts())
-            .get("/me", async ({ headers, set }) => {
-                const token = headers['authorization']; 
-                const result = await me({ token });
-                if (result.status) { 
-                    set.status = result.status;
-                }
-                return result;
-            })
-        )
-        
-        .group("/product", (app) =>
-            app
-            .get("/get", async () => await getProduct())
-            .post("/add", async ({ body }) => await addProduct(body))
-            .put("/put", async ({ body }) => await putProduct(body))
-            .delete("/del", async ({ body }) => await delProduct(body))
-        )
+          .post("/login", async ({ body }) => await login(body))
+          .post("/register", async ({ body }) => await register(body))
+          .get(
+            "/verify-email",
+            async ({ query }) => await verifyEmail(query.token),
+          )
+          .get("/accounts", async () => await getAccounts())
+          .get("/me", async ({ headers, set }) => {
+            const token = headers["authorization"];
+            const result = await me({ token });
+            if (result.status) {
+              set.status = result.status;
+            }
+            return result;
+          }),
+      )
 
-        .group("/contact", (app) =>
-            app
-                .post("/send", async ({ body }) => sendContactMail(body))
-                // .post("/whatsapp", async ({ body }) => sendWhatsappMessage(body))
-        )
-    )
+      .group("/product", (app) =>
+        app
+          .get("/get", async () => await getProduct())
+          .post("/add", async ({ body }) => await addProduct(body))
+          .put("/put", async ({ body }) => await putProduct(body))
+          .delete("/del", async ({ body }) => await delProduct(body)),
+      )
 
-    .onError(({ code, set, error }) => {
-        if (code === "NOT_FOUND") {
-            set.status = 404;
-            return { error: "Endpoint tidak ditemukan" };
-        }
-        console.error("Global Error:", error);
-        return {
-            status: 500,
-            error: error.message,
-        };
-    });
+      .group(
+        "/contact",
+        (app) => app.post("/send", async ({ body }) => sendContactMail(body)),
+        // .post("/whatsapp", async ({ body }) => sendWhatsappMessage(body))
+      ),
+  )
+
+  .onError(({ code, set, error }) => {
+    if (code === "NOT_FOUND") {
+      set.status = 404;
+      return { error: "Endpoint tidak ditemukan" };
+    }
+    console.error("Global Error:", error);
+    return {
+      status: 500,
+      error: error.message,
+    };
+  });
 
 export default app;
 
